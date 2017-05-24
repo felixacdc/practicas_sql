e1cffb7c070ae8f8cdc443b48b286c6c461599cd
felixacdc <pfmata01@gmail.com>
2017-05-24 10:21:31 -0600

    [add] oracle quinta parte

--- Oracle/stored_procedures.sql
+++ Oracle/stored_procedures.sql
@@ -91,3 +91,37 @@ END;
 	LOOP
 	   monthly_value := daily_value * 31;
 	END LOOP;
+
+-- Excepciones
+	DECLARE
+ 	-- Declaramos una excepcion identificada por VALOR_NEGATIVO
+    VALOR_NEGATIVO EXCEPTION;
+    valor NUMBER;
+	BEGIN
+	  -- Ejecucion
+	valor := -1;
+		IF valor < 0 THEN
+			RAISE VALOR_NEGATIVO;
+		END IF;
+	EXCEPTION
+	  -- Excepcion
+	WHEN VALOR_NEGATIVO THEN
+		dbms_output.put_line('El valor no puede ser negativo');
+	END; 
+	---------------------
+	DECLARE
+	  err_num NUMBER;
+	  err_msg VARCHAR2(255);
+	  result  NUMBER;
+	BEGIN
+	  SELECT 1/0 INTO result 
+	  FROM DUAL;
+	  
+	EXCEPTION  
+	WHEN OTHERS THEN
+	  
+	  err_num := SQLCODE;
+	  err_msg := SQLERRM;
+	  DBMS_OUTPUT.put_line('Error:'||TO_CHAR(err_num));
+	  DBMS_OUTPUT.put_line(err_msg);
+	END;
